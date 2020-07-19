1b6e04c22ff1ce6b4bfd74d6773c8ab635b83cb4
Lyubomira Lyubomirova <lyubomira.lyubomirova@fadata.eu>
2020-07-19 18:07:19 +0300

    separating our app into client folder, added express server to help handle backend stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_KbmsQgaQVbzP5vGhYF0EYkMn00IXjq4PTW";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert("There was an issue with your payment.");
+      });
   };
 
   return (
